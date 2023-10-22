510fad271943dcd242cca8fb34f9cbd0a3c6ffd3
DxDelis <nikitapopov1193@gmail.com>
2023-10-22 20:46:04 +0300

    com1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Items from './components/items';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id:1,
+          title: 'Худи Gap',
+          img: 'gap.jpg',
+          category: 'hoodies',
+          price: '6400'
+        }
+        
+      ]
+    }
+  }
+  render() {
+    return(
+      <div className="wrapper">
+      <Header />
+      <Items items = {this.state.items} />
+      <Footer /> 
+      </div>
+    )
+  }
 }
 
 export default App;
